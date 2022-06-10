69c085f4bb33e48492f29a42b8b47fa594cfe0bb
xelva <xelvacodes@gmail.com>
2022-06-10 12:11:30 -0500

    auth

--- services/passport.js
+++ services/passport.js
@@ -21,17 +21,15 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-    }, (accessToken, refreshToken, profile, done) => {
+    }, async (accessToken, refreshToken, profile, done) => {
 
-        User.findOne({ googleId: profile.id })
-            .then((existingUser) => {
-                if (existingUser) {
-                    done(null, existingUser);
-                } else {
-                    new User({ googleId: profile.id })
-                        .save()
-                        .then(user => done(null, user))
-                }
-            })
+        const existingUser = await User.findOne({ googleId: profile.id })
+            
+        if (existingUser) {
+            return done(null, existingUser);
+        } 
+
+        const user = await new User({ googleId: profile.id }).save()
+             done(null, user)
     })
 );
