63c99fdaf568544d3a485a703b02fb24f0745d57
xelva <xelvacodes@gmail.com>
2022-06-11 11:02:00 -0500

    stripe

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,7 @@
 const passport = require('passport');
+const Stripe = require('stripe');
+
+const stripe = new Stripe(process.env.stripeSecretKey)
 
 module.exports = (app) => {
     //kick user into the passport flow
@@ -9,7 +12,13 @@ module.exports = (app) => {
     );
 
     //confirm the user with the returned code
-    app.get('/auth/google/callback', passport.authenticate('google'));
+    app.get(
+        '/auth/google/callback', 
+        passport.authenticate('google'),
+        (req, res) => {
+            res.redirect('/surveys')
+        }
+    );
 
     app.get('/api/current_user', (req, res) => {
         res.send(req.user)
@@ -19,6 +28,23 @@ module.exports = (app) => {
         req.logout(err => {
             if (err) { return next(err); }
         });
-        res.send(req.user)
+        res.redirect('/')
+        
+    })
+
+    app.post('/api/payment_intents', async (req, res) => {
+        try {
+            const { amount } = req.body;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount,
+                currency: "usd"
+            });
+
+            res.status(200).send(paymentIntent.client_secret);
+            console.log(paymentIntent.client_secret)
+        } catch (err) {
+            res.status(500).json({ statusCode: 500, message: err.message })
+        }
+
     })
 };
