1e2e673e13c6aa32cebf5b78fa44772464ee0131
xelva <xelvacodes@gmail.com>
2022-06-11 12:24:39 -0500

    added client

--- index.js
+++ index.js
@@ -25,6 +25,16 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 //let heroku define the port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
